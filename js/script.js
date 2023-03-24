5217087d5a2053bf52ea30283d3073381d4f0454
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2023-03-24 17:11:28 -0300

    easter egg

--- js/script.js
+++ js/script.js
@@ -1,63 +1,63 @@
 const onDisplay = document.getElementById('operations')
-let operations=''
+let operations = ''
 
-function insert(valor) { 
-  operations += valor;
+function insert(valor) {
+  operations += valor
   console.log('operations: ', operations)
-  
-  if ( valor == '/' || valor == '*'){
-    
-    const parcialOperation= operations.substring(0, operations.length -1);
 
-    const parcialResult= eval(parcialOperation);
+  if (valor == '/' || valor == '*') {
+    const parcialOperation = operations.substring(0, operations.length - 1)
 
-    operations= parcialResult + valor;
-    
-    valor= parcialResult + changeSynmbol(valor);
-    
-    onDisplay.innerHTML= '';
+    const parcialResult = eval(parcialOperation)
+
+    operations = parcialResult + valor
+
+    valor = parcialResult + changeSynmbol(valor)
+
+    onDisplay.innerHTML = ''
   }
 
-  if (valor =='/100'){
-    valor= changeSynmbol(valor);
+  if (valor == '/100') {
+    valor = changeSynmbol(valor)
   }
   changeSynmbol(valor)
 
   onDisplay.innerHTML += valor
-
 }
 
-function changeSynmbol(valor){
-  if(valor == '/'){
-    return valor = '÷'
+function changeSynmbol(valor) {
+  if (valor == '/') {
+    return (valor = '÷')
   }
 
-  if(valor == '*'){
-    return valor = 'x'
+  if (valor == '*') {
+    return (valor = 'x')
   }
 
-  if(valor == '/100'){
-    return valor = '%'
-  } 
-}
-
-function clean(){
-  onDisplay.innerHTML= '';
-  operations= '';
-  
-}
-
-function backspace(){
-  if(onDisplay.textContent){
-    let changeDisplay= document.querySelector('.display').innerHTML;
-
-    onDisplay.innerHTML= changeDisplay.substring(0, operations.length -1);
-
-    operations= operations.substring(0, operations.length -1)
+  if (valor == '/100') {
+    return (valor = '%')
   }
-
 }
 
-function result(){
-  onDisplay.innerHTML = eval(operations);
+function clean() {
+  onDisplay.innerHTML = ''
+  operations = ''
+}
+
+function backspace() {
+  if (onDisplay.textContent) {
+    let changeDisplay = document.querySelector('.display').innerHTML
+
+    onDisplay.innerHTML = changeDisplay.substring(0, operations.length - 1)
+
+    operations = operations.substring(0, operations.length - 1)
+  }
+}
+
+function result() {
+  onDisplay.innerHTML = eval(operations)
+
+  if (operations == '21092019') {
+    onDisplay.innerHTML = '♥ Alê & Yago ♥ Amor eterno!'
+  }
 }
